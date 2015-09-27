0899d61a87cf2bea951c8018bc886982e76f10dc
Ned <needarb@gmail.com>
2015-09-27 16:38:31 -0500

    Commented Cell

--- src/Board.java
+++ src/Board.java
@@ -4,19 +4,41 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
 /**
- * Created by needa_000 on 7/7/2014.
+ * Created by Ned Read on 7/7/2014.
+ *
+ * The panel containing the grid of cells. Handles dragging, highlighting, and adding or replacing a cell.
  */
 public class Board extends JPanel implements KeyListener, MouseListener, MouseMotionListener
 {
+    //region Constants
     public static final int NUM_COL = 8;
     public static final int NUM_ROW = 5;
+    //endregion
+
+    //region Private Members
     private MainWindow main;
     private GroupHighlighter groupHighlighter;
     private BufferedImage backBufferContainer;
     private Graphics2D backBuffer;
+    private Cell lastCellHighlighted;
 
     private Cell[][] grid;
 
+    //region Dragging Private Members
+    private boolean isDragging = false;
+    private int draggingOffsetX;
+    private int draggingOffsetY;
+    private Cell beingDragged;
+    private Cell swapped;
+    private int startingX;
+    private int startingY;
+    //endregion
+    //endregion
+
+    /**
+     * Create the board panel. Creates cell grid. Sets all values to the default (Natural)
+     * @param main The MainWindow the board is placed in
+     */
     public Board(MainWindow main)
     {
         super();
@@ -29,7 +51,6 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
 
         backBufferContainer = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_RGB);
         backBuffer = (Graphics2D) backBufferContainer.getGraphics();
-        System.out.println(backBuffer.getClass());
         grid = new Cell[NUM_COL][NUM_ROW];
         for (int x = 0; x < grid.length; x++)
         {
@@ -45,11 +66,15 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         setVisible(true);
     }
 
+    /**
+     * Paints the grid and the cell being dragged. Double Buffered.
+     * @param g Graphics from the MainWindow
+     */
     @Override
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        long currentTime = System.currentTimeMillis();
+
         backBuffer.setColor(Color.LIGHT_GRAY);
         backBuffer.fillRect(0, 0, this.getWidth(), this.getHeight());
 
@@ -61,10 +86,11 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
             beingDragged.paint(backBuffer);
 
         g.drawImage(backBufferContainer, 0, 0, null);
-        System.out.println("Time to paint board: " + (System.currentTimeMillis() - currentTime));
-
     }
 
+    /**
+     * Paint each cell onto the back buffer.
+     */
     public void drawGrid()
     {
         for (int x = 0; x < grid.length; x++)
@@ -72,6 +98,10 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
                 grid[x][y].paint(backBuffer);
     }
 
+    /**
+     * Mouse Released Event. Used for handling dragging and changing cell type.
+     * @param e Event
+     */
     @Override
     public void mouseReleased(MouseEvent e)
     {
@@ -99,9 +129,10 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         }
     }
 
-
-    private Cell lastCellHighlighted;
-
+    /**
+     * MouseMoved event. Used to handle cell dragging and cell highlighting.
+     * @param e Event
+     */
     @Override
     public void mouseMoved(MouseEvent e)
     {
@@ -115,6 +146,12 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
             repaint();
     }
 
+    /**
+     * Handles cell highlighting. Changes the highlighted cell if necessary.
+     * @param x Column in grid
+     * @param y Row in grid
+     * @return Whether or not something changed, indicating the necessity of a repaint.
+     */
     public boolean processHighlight(int x, int y)
     {
         if (isDragging)
@@ -146,6 +183,11 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         return true;
     }
 
+    /**
+     * Handles the dragging of a cell. Swaps the cells if drags over a different cell.
+     * @param e Event
+     * @return Whether or not a repaint is necessary.
+     */
     public boolean processDrag(MouseEvent e)
     {
         if (!isDragging)
@@ -153,15 +195,15 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
 
         int x = e.getX() / Cell.WIDTH;
         int y = e.getY() / Cell.HEIGHT;
+
         if (!cordsInGrid(x, y))
             return false;
+
         if (!grid[x][y].equals(beingDragged))
         {
             if (swapped != null)
                 swapped.setHighlighted(false);
 
-            swappedX = x;
-            swappedY = y;
             swapped = grid[x][y];
             swapped.setHighlighted(true, Cell.STARTING_COLOR);
 
@@ -171,7 +213,6 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
                 if (!(x == startingX && y == startingY))
                 {
                     swapTiles(startingX, startingY, x, y);
-                    //grid[startingX][startingY].setHighlighted(true, Cell.STARTING_COLOR);
                 }
             }
             swapTiles(x, y, beingDragged.getGridX(), beingDragged.getGridY());
@@ -183,6 +224,13 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
 
     }
 
+    /**
+     * Swaps the cells at the given coordinates.
+     * @param x Column of first cell
+     * @param y Row of first cell
+     * @param x2 Column of second cell
+     * @param y2 Row of second cell
+     */
     public void swapTiles(int x, int y, int x2, int y2)
     {
         Cell temp = grid[x][y];
@@ -194,6 +242,12 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         grid[x2][y2].setLocation(x2 * Cell.WIDTH, y2 * Cell.HEIGHT);
     }
 
+    /**
+     * Used for ensuring coordinates are in the grid
+     * @param x Column of cell
+     * @param y Row of cell
+     * @return Whether the given coordinates are within the grid or not
+     */
     public boolean cordsInGrid(int x, int y)
     {
         if (x >= grid.length || x < 0)
@@ -203,13 +257,10 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         return true;
     }
 
-
-    @Override
-    public void mouseEntered(MouseEvent e)
-    {
-
-    }
-
+    /**
+     * Used to handle highlighting. Unhighlights last cell when the mouse leaves the board.
+     * @param e
+     */
     @Override
     public void mouseExited(MouseEvent e)
     {
@@ -219,12 +270,11 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         repaint();
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e)
-    {
 
-    }
-
+    /**
+     * Used in handling dragging. Used to begin the drag.
+     * @param e Event
+     */
     @Override
     public void mousePressed(MouseEvent e)
     {
@@ -241,49 +291,30 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
         }
     }
 
-    @Override
-    public void keyTyped(KeyEvent e)
+
+    /**
+     * Redraw the image on all of the cells.
+     */
+    public void updateAllCells()
     {
+        for (Cell[] col : grid)
+            for (Cell c : col) c.updateImage();
     }
 
-    @Override
-    public void keyReleased(KeyEvent e)
-    {
-    }
-
-    public static void pause(long millis)
-    {
-        try
-        {
-            Thread.sleep(millis);
-        } catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean isDragging = false;
-    private int draggingOffsetX;
-    private int draggingOffsetY;
-    private Cell beingDragged;
-    private Cell swapped;
-    private int swappedX;
-    private int swappedY;
-    private int startingX;
-    private int startingY;
-
-
-    public Cell[][] getGrid()
-    {
-        return grid;
-    }
-
-
+    /**
+     * Reset all of the cells back to the default: Only one wild.
+     */
     public void reset()
     {
         for (Cell[] r : grid)
             for (Cell c : r)
                 c.replaceStack(Cell.WILD);
+    }
+
+    //region Getters
+    public Cell[][] getGrid()
+    {
+        return grid;
     }
 
     public GroupHighlighter getGroupHighlighter()
@@ -295,21 +326,39 @@ public class Board extends JPanel implements KeyListener, MouseListener, MouseMo
     {
         return main;
     }
+    //endregion
 
-    public void updateAllCells()
-    {
-        for (Cell[] col : grid)
-            for (Cell c : col) c.updateImage();
-    }
-
+    //region Unused Events
     @Override
     public void mouseDragged(MouseEvent e)
     {
         mouseMoved(e);
+    }
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
     }
 
     @Override
     public void keyPressed(KeyEvent e)
     {
     }
+    //endregion
 }
--- src/Cell.java
+++ src/Cell.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
- * Created by needa_000 on 7/6/2014.
+ * Created by Ned Read on 7/6/2014.
+ * Represents one cell on the board. Has methods for adding and removing to/from the stack.
+ * Generates the image for the cell, handling the border, image/color, and the stack label.
  */
 public class Cell extends JPanel
 {
+    //region Constants
     public static final String[] TILE_NAMES = {"Natural", "Farm", "Solar", "House", "Business"};
     public static final String[] RESOURCE_NAMES= {"Natural", "Food", "Energy", "Housing", "Money"};
     public static final int WILD = 0;
@@ -17,10 +20,12 @@ public class Cell extends JPanel
     public static final int HOUSE = 3;
     public static final int BUSINESS = 4;
 
-    //public static final Color[] COLORS = {new Color(0x013220), new Color(0x8CC739), new Color(0x20B2AA), new Color(Color.YELLOW.getRGB()), new Color(0x999999)};
+
     public static final BufferedImage[] IMAGES;
+    //public static final Color[] COLORS = {new Color(0x013220), new Color(0x8CC739), new Color(0x20B2AA), new Color(Color.YELLOW.getRGB()), new Color(0x999999)}; //Grey's Colors
     public static final Color[] COLORS = {new Color(0x013220), new Color(0xCCCC00), new Color(0x20B2AA), new Color(0x8CC739), new Color(0x0F3B5F)}; //Sim City Colors
 
+    public static final Font LABEL_FONT;
     public static final Color HIGHLIGHT_COLOR = Color.MAGENTA;
     public static final Color DRAGGED_COLOR = Color.RED;
     public static final Color STARTING_COLOR = new Color(0x00FF00);
@@ -34,6 +39,7 @@ public class Cell extends JPanel
     public static final int BORDER_SIZE = 4;
     public static final Color LABEL_COLOR = Color.BLACK;
 
+
     static
     {
         IMAGES = new BufferedImage[TILE_NAMES.length];
@@ -43,10 +49,12 @@ public class Cell extends JPanel
             IMAGES[i] = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
             IMAGES[i].getGraphics().drawImage(image.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH), 0, 0, null);
         }
+
+        LABEL_FONT = new Font("Verdana", Font.BOLD, LABEL_SIZE);
     }
+    //endregion
 
-
-
+    //region Private Members
     private BufferedImage onScreenImage;
     private JLabel stackLabel;
     private Board board;
@@ -55,12 +63,26 @@ public class Cell extends JPanel
     private int gridY;
     private boolean isHighlighted;
     private Color highlightColor;
+    //endregion
 
-    public Cell(Board board, int x, int y)
+    /**
+     * Creates a cell with the default state (Natural) at the given coordinates.
+     * @param board Board the cell is created in
+     * @param gridX Column in board
+     * @param gridY Row in board
+     */
+    public Cell(Board board, int gridX, int gridY)
     {
-        this(board, Cell.WILD, x, y);
+        this(board, Cell.WILD, gridX, gridY);
     }
 
+    /**
+     * Creates a cell with the given state at the given coordinates.
+     * @param board Board the cell is created in
+     * @param state Beginning state for the cell.
+     * @param gridX Column in board
+     * @param gridY Row in board
+     */
     public Cell(Board board, int state, int gridX, int gridY)
     {
         this.board = board;
@@ -74,6 +96,10 @@ public class Cell extends JPanel
         setToolTipText("THIS IS TEXT " + Math.random());
     }
 
+    /**
+     * Replace the cell's stack with the given state
+     * @param newState The state to replace the stack with
+     */
     public void replaceStack(int newState)
     {
         if (newState < 0 || newState > 4)
@@ -86,6 +112,9 @@ public class Cell extends JPanel
         updateImage();
     }
 
+    /**
+     * Re-draw the image displaced on the board for this cell
+     */
     public void updateImage()
     {
         Graphics2D g = (Graphics2D) onScreenImage.getGraphics();
@@ -94,17 +123,45 @@ public class Cell extends JPanel
             drawCellImage(g);
         else
             drawCellColor(g);
+
         drawCellBorder(g);
         drawCellStackLabel(g);
     }
 
 
-    public void updateBorder()
+    /**
+     * Re-draws the cell boarder.
+     */
+    public void drawCellBorder()
     {
         Graphics2D g = (Graphics2D) onScreenImage.getGraphics();
         drawCellBorder(g);
     }
 
+    /**
+     * Draws the cell boarder on the given graphics object.
+     * Uses the appropriate color if highlighted.
+     * @param g Graphic object to draw the boarder on
+     */
+    private void drawCellBorder(Graphics2D g)
+    {
+        g.setStroke(new BasicStroke(BORDER_SIZE));
+        if (isHighlighted)
+            g.setColor(highlightColor);
+        else
+        {
+            if (!board.getMain().getOptionsWindow().shouldDisplayGrid())
+                return;
+            g.setColor(DEFAULT_BORDER_COLOR);
+        }
+        g.drawRect((int)(BORDER_SIZE/2), (int)(BORDER_SIZE/2), WIDTH - BORDER_SIZE, HEIGHT - BORDER_SIZE);
+    }
+
+    /**
+     * Draws a rectangle with the cells color on the given Graphic object
+     * Used when the the application is not displaying cell images
+     * @param g Graphic object to draw the rectangle on
+     */
     private void drawCellColor(Graphics2D g)
     {
         g.setColor(COLORS[stack.getFirst()]);
@@ -120,40 +177,32 @@ public class Cell extends JPanel
         }
     }
 
+    /**
+     * Draws the cell images on the given graphic object
+     * Used when the the application is displaying cell images
+     * @param g Graphic object to draw the image on
+     */
     private void drawCellImage(Graphics2D g)
     {
         g.drawImage(IMAGES[stack.getLast()],0,0,null);
     }
 
-    private void drawCellBorder(Graphics2D g)
-    {
-        g.setStroke(new BasicStroke(BORDER_SIZE));
-        if (isHighlighted)
-            g.setColor(highlightColor);
-        else
-        {
-            if (!board.getMain().getOptionsWindow().shouldDisplayGrid())
-                return;
-            g.setColor(DEFAULT_BORDER_COLOR);
-        }
-        g.drawRect((int)(BORDER_SIZE/2), (int)(BORDER_SIZE/2), WIDTH - BORDER_SIZE, HEIGHT - BORDER_SIZE);
-    }
-
+    /**
+     * Draws the stack label on the given graphic object.
+     * Only draws a label if the stack is greater than or equal to 2.
+     * Draws "X<Stack Height>"
+     * @param g
+     */
     private void drawCellStackLabel(Graphics2D g)
     {
         if (stack.size() >= 2)
         {
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g.setFont(new Font("Verdana", Font.BOLD, LABEL_SIZE));
+            g.setFont(LABEL_FONT);
             g.setColor(LABEL_COLOR);
             g.drawString("X" + stack.size(), LABEL_WIDTH_OFFSET, LABEL_HEIGHT_OFFSET);
 
         }
-    }
-
-    public BufferedImage getOnScreenImage()
-    {
-        return onScreenImage;
     }
 
     @Override
@@ -161,6 +210,99 @@ public class Cell extends JPanel
     {
         super.paint(g);
         g.drawImage(onScreenImage, getX(), getY(), null);
+    }
+
+    /**
+     * Remove the top from the stack. Does nothing if the stack only contains one element
+     * Updates the image accordingly.
+     */
+    public void removeTop()
+    {
+        if (stack.size() == 0)
+        {
+            System.err.println("Stack empty!!");
+            return;
+        } else if (stack.size() == 1)
+        {
+            System.err.println("Stack only contains one element!!");
+            return;
+        }
+        stack.remove(stack.size() - 1);
+        updateImage();
+    }
+
+
+    /**
+     * Adds the current state to the stack or replaces the stack with the current state, depending on the stack
+     * and the state being added. Houses and Businesses can be stacked and capped with a Farm or Solar. Does not
+     * replace a capped stack if one tried to add another farm or solar or house or business to it. Bulldozer removed
+     * one element from the stack, and replaces the final element with a natural area.
+     * @param state State to add or replace
+     */
+    public void addOrReplaceStack(int state)
+    {
+        if(state == Toolbar.MOVE)
+            return;
+        if(state == WILD) //Natural Area's cannot be stacked
+        {
+            replaceStack(state);
+            return;
+        }
+        if (state == HOUSE || state == BUSINESS)
+        {
+            if(stack.getFirst() == FARM || stack.getFirst() == SOLAR || stack.getFirst() == WILD) //House and Business cannot go on top of Farm, Solar, or Natural
+            {
+                replaceStack(state);
+                return;
+            }
+            if(stack.getLast() == FARM || stack.getLast() == SOLAR) //Business or House Capped with Farm or Solar: Don't replace
+                return;
+            addToStack(state);
+            return;
+
+        }
+        if (state == FARM || state == SOLAR)
+        {
+            if(stack.getLast() == BUSINESS || stack.getLast() == HOUSE) //Farm and Solar can be placed on top of Business or House
+            {
+                addToStack(state);
+                return;
+            }
+            if(stack.getFirst() == BUSINESS || stack.getFirst() == HOUSE) //Don't replace a stack which is already capped with Farm or Solar
+                return;
+            replaceStack(state);
+            return;
+        }
+        if (state == Toolbar.BULLDOZER) //Delete on from stack or replace with Natural
+        {
+            if (stack.size() == 1)
+                replaceStack(Cell.WILD);
+            else
+                removeTop();
+            return;
+        }
+        addToStack(state);
+    }
+
+    /**
+     * Add the given state to the stack. Does not check if the state is valid or can be placed on the current
+     * stack
+     * @param state State to add to the stack.
+     */
+    public void addToStack(int state)
+    {
+        stack.add(state);
+        updateImage();
+    }
+
+    //region Getters
+    /**
+     * The image that should be displayed onscreen for the cell
+     * @return
+     */
+    public BufferedImage getOnScreenImage()
+    {
+        return onScreenImage;
     }
 
     public int getGridX()
@@ -176,18 +318,6 @@ public class Cell extends JPanel
     public boolean isHighlighted()
     {
         return isHighlighted;
-    }
-
-    public void setHighlighted(boolean isHighlighted)
-    {
-        setHighlighted(isHighlighted, this.highlightColor == null ? HIGHLIGHT_COLOR : this.highlightColor);
-    }
-
-    public void setHighlighted(boolean isHighlighted, Color highlightColor)
-    {
-        this.isHighlighted = isHighlighted;
-        this.highlightColor = highlightColor;
-        updateBorder();
     }
 
     public LinkedList<Integer> getStack()
@@ -210,76 +340,23 @@ public class Cell extends JPanel
         return neighbors;
     }
 
-    public void addToStack(int state)
-    {
-        stack.add(state);
-        updateImage();
-    }
 
-    public void removeTop()
+
+    //endregion
+
+    //region Setters
+    public void setHighlighted(boolean isHighlighted, Color highlightColor)
     {
-        if (stack.size() == 0)
-        {
-            System.err.println("Stack empty!!");
-            return;
-        } else if (stack.size() == 1)
-        {
-            System.err.println("Stack only contains one element!!");
-            return;
-        }
-        stack.remove(stack.size() - 1);
-        updateImage();
+        this.isHighlighted = isHighlighted;
+        this.highlightColor = highlightColor;
+        drawCellBorder();
     }
 
 
-    public JLabel getLabel()
-    {
-        return stackLabel;
-    }
 
-    public void addOrReplaceStack(int state)
+    public void setHighlighted(boolean isHighlighted)
     {
-        if(state == Toolbar.MOVE)
-            return;
-        if(state == WILD)
-        {
-            replaceStack(state);
-            return;
-        }
-        if (state == HOUSE || state == BUSINESS)
-        {
-            if(stack.getFirst() == FARM || stack.getFirst() == SOLAR || stack.getFirst() == WILD)
-            {
-                replaceStack(state);
-                return;
-            }
-            if(stack.getLast() == FARM || stack.getLast() == SOLAR)
-                return;
-            addToStack(state);
-            return;
-
-        }
-        if (state == FARM || state == SOLAR)
-        {
-            if(stack.getLast() == BUSINESS || stack.getLast() == HOUSE)
-            {
-                addToStack(state);
-                return;
-            }
-            if(stack.getFirst() == BUSINESS || stack.getFirst() == HOUSE)
-                return;
-            replaceStack(state);
-            return;
-        }
-        if (state == Toolbar.BULLDOZER)
-        {
-            if (stack.size() == 1)
-                replaceStack(Cell.WILD);
-            else
-                removeTop();
-            return;
-        }
-        addToStack(state);
+        setHighlighted(isHighlighted, this.highlightColor == null ? HIGHLIGHT_COLOR : this.highlightColor);
     }
 
     public void setGridLocation(int newGridX, int newGridY)
@@ -287,4 +364,5 @@ public class Cell extends JPanel
         gridX = newGridX;
         gridY = newGridY;
     }
+    //endregion
 }
